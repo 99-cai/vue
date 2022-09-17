f37105af2421ba219e79ffcdf822059f468e99fb
cai <1270442324@qq.com>
2022-09-17 17:12:39 +0800

    第三次

--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -1,25 +1,68 @@
 <template>
   <div style="padding:30px;">
     <div>
-      <el-form :inline="true" >
-        <el-form-item label="作物成熟时间(单位:  秒,xx秒后成熟)">
-          <el-select v-model="time">
-            <el-option label="10" value="0"></el-option>
-            <el-option label="20" value="1"></el-option>
-            <el-option label="30" value="2"></el-option>
-          </el-select>
+      <el-form :inline="true" :model="form" class="demo-form-inline">
+        <el-form-item label="Uid" width="80px" prop="id">
+          <el-input v-model="form.id" placeholder="用户ID"></el-input>
+        </el-form-item>
+        <el-form-item label="作物成熟时间">
+          <el-input v-model="form.time" placeholder="(单位:  秒,xx秒后成熟)" style="width:290px"></el-input>
         </el-form-item>
         <el-form-item label="解锁新的种子">
-          <el-select v-model="newseed">
-            <el-option label="萝卜" value="0"></el-option>
-            <el-option label="茄子" value="1"></el-option>
-            <el-option label="玉米" value="2"></el-option>
-          </el-select>
-        </el-form-item>     
+          <el-input v-model="form.newneed" placeholder="种子id" style="width:290px"></el-input>
+        </el-form-item>
+        <!-- 添加 -->
+        <div style="float:right">
+          <el-button type="primary" @click="add" style="margin-right:20px">重置</el-button>
+          <el-button type="primary" @click="submit">添 加</el-button>
+        </div>
       </el-form>
-      <el-button  type="primary" @click="add">添加道具</el-button>
+      <!-- 添加弹框 -->
+      <el-dialog style="width: 1000px;height: 1000px;" title="添加道具" :visible.sync="zdydialog">
+        <el-form :model="form" :label-position="labelPosition">
+          <el-form-item label="Uid" prop="id">
+            <el-input v-model="form.id" placeholder="用户ID" style="width:220px"></el-input>
+          </el-form-item>
+          <el-form-item label="作物成熟时间">
+            <el-input v-model="form.time" placeholder="(单位:  秒,xx秒后成熟)" style="width:290px"></el-input>
+          </el-form-item>
+          <el-form-item label="解锁新的种子">
+            <el-input v-model="form.newneed" placeholder="种子id" style="width:290px"></el-input>
+          </el-form-item>
+        </el-form>
+
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="cancel">取 消</el-button>
+          <el-button type="primary" @click="submit">确 定</el-button>
+        </div>
+      </el-dialog>
+
+      <!-- 表单 -->
+      <div class="tableMain">
+        <el-table :data="tableData" style="width: 100%" :cell-style="{ textAlign: 'center' }"
+          :header-cell-style="{textAlign: 'center'}">
+          <el-table-column prop="id" label="Uid" style="width:220px">
+          </el-table-column>
+          <el-table-column prop="time" label="作物成熟时间" style="width:320px">
+            <template slot-scope="scope">
+              {{scope.row.time}}秒后成熟
+            </template>
+          </el-table-column>
+          <el-table-column prop="newneed" label="解锁种子ID" style="width:300px">
+          </el-table-column>
+          <!-- 添加弹框 -->
+          <el-table-column label="操作">
+            <template slot-scope="scope2">
+              <el-button size="small" @click="handleEdit(scope2.$index, scope2.row)">编辑
+              </el-button>
+              <el-button size="small" type="danger" @click="handleDelete(scope2.$index, scope2.row)">删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </div>
-<!-- 子路由 -->
+    <!-- 子路由 -->
     <router-view />
   </div>
 </template>
@@ -27,11 +70,71 @@
 export default {
   data() {
     return {
-     
+      labelPosition: 'top',
+      form: {
+        id: '',
+        time: '',
+        newneed: '',
+      },
+      tableData: [{
+        id: '0',
+        time: '20',
+        newneed: '3',
+      },],
+      submitType: "",
+      zdydialog: false,
+      loading: true,
     }
   },
-  methods: {
 
-  }
+  methods: {
+    add() {
+      this.form = {
+        id: '',
+        time: '',
+        newneed: ''
+      }
+      this.zdydialog = false
+      this.submitType = "add"
+    },
+    submit() {
+      if (this.submitType == "add") {
+        this.tableData.push(this.form)//向tableData中添加数据
+        this.zdydialog = false
+      } else {
+
+      }
+    },
+    cancel() {
+      this.zdydialog = false
+    },
+    handleEdit(index, row) {
+      this.form = row     //将该行对象数据直接赋给form
+      this.zdydialog = false //自定义对话框展示
+      this.submitType = "update";
+    },
+    handleDelete(index, row) {
+      this.$confirm('此操作将删除改操作, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.tableData.splice(index, 1)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+
+
+
+  },
+
 }
 </script>
--- src/views/nested/menu3/index.vue
+++ src/views/nested/menu3/index.vue
@@ -6,61 +6,75 @@
           <el-input v-model="form.id" placeholder="用户ID"></el-input>
         </el-form-item>
         <el-form-item label="装盘抽奖券">
-          <el-select v-model="form.raffleticket">
+          <el-input v-model="form.raffleticket" placeholder="格式[10,20,30],对应关系[绿券,紫券,橙券]" style="width:290px">
+          </el-input>
+
+          <!-- <el-select v-model="form.raffleticket">
             <el-option label="10" value="10"></el-option>
             <el-option label="20" value="20"></el-option>
             <el-option label="30" value="30"></el-option>
-          </el-select>
+          </el-select> -->
         </el-form-item>
-        <el-form-item label="对应品级">
+        <!-- <el-form-item label="对应品级">
           <el-select v-model="form.quality">
             <el-option label="绿券" value="绿券"></el-option>
             <el-option label="紫券" value="紫券"></el-option>
             <el-option label="橙券" value="橙券"></el-option>
           </el-select>
-        </el-form-item>
+        </el-form-item> -->
+        <!-- 添加 -->
+        <div style="float:right">
+          <el-button type="primary" @click="add" style="margin-right:20px">重置</el-button>
+          <el-button type="primary" @click="submit">添 加</el-button>
+        </div>
       </el-form>
-      <!-- 添加 -->
-      <el-button type="primary" @click="add" style="margin-right:20px">重置</el-button>
-
-      <el-button type="primary" @click="submit">添 加</el-button>
       <!-- 添加弹框 -->
       <el-dialog style="width: 1000px;height: 1000px;" title="添加道具" :visible.sync="zdydialog">
-      <el-form :model="form" :label-position="labelPosition">
-        <el-form-item label="Uid" prop="id">
-          <el-input  v-model="form.id" placeholder="用户ID" style="width:220px"></el-input>
-        </el-form-item>
-        <el-form-item label="装盘抽奖券">
-          <el-select v-model="form.raffleticket" >
-            <el-option label="10" value="10"></el-option>
-            <el-option label="20" value="20"></el-option>
-            <el-option label="30" value="30"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="对应品级">
-          <el-select v-model="form.quality">
-            <el-option label="绿券" value="绿券"></el-option>
-            <el-option label="紫券" value="紫券"></el-option>
-            <el-option label="橙券" value="橙券"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
+        <el-form :model="form" :label-position="labelPosition">
+          <el-form-item label="Uid" prop="id">
+            <el-input v-model="form.id" placeholder="用户ID" style="width:220px"></el-input>
+          </el-form-item>
+          <el-form-item label="装盘抽奖券">
+            <el-select v-model="form.raffleticket">
+              <el-option label="10" value="10"></el-option>
+              <el-option label="20" value="20"></el-option>
+              <el-option label="30" value="30"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="对应品级">
+            <el-select v-model="form.quality">
+              <el-option label="绿券" value="绿券"></el-option>
+              <el-option label="紫券" value="紫券"></el-option>
+              <el-option label="橙券" value="橙券"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
 
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="cancel">取 消</el-button>
-        <el-button type="primary" @click="submit">确 定</el-button>
-      </div>
-    </el-dialog>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="cancel">取 消</el-button>
+          <el-button type="primary" @click="submit">确 定</el-button>
+        </div>
+      </el-dialog>
 
 
       <!-- 表单 -->
       <div class="tableMain">
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column prop="id" label="Uid" width="180">
+        <el-table :data="tableData" style="width: 100%" :cell-style="{ textAlign: 'center' }"
+          :header-cell-style="{textAlign: 'center'}">
+          <el-table-column prop="id" label="Uid" style="width:30%">
           </el-table-column>
-          <el-table-column prop="raffleticket" label="装盘抽奖券" width="180">
+          <el-table-column prop="raffleticket" label="装盘抽奖券" style="width:33%">
           </el-table-column>
-          <el-table-column prop="quality" label="对应品级" width="180">
+          <!-- <el-table-column prop="quality" label="对应品级" style="width:33%">
+          </el-table-column> -->
+          <!-- 添加弹框 -->
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑
+              </el-button>
+              <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除
+              </el-button>
+            </template>
           </el-table-column>
         </el-table>
       </div>
@@ -74,55 +88,103 @@
 export default {
   data() {
     return {
-      labelPosition:'top',
+      labelPosition: 'top',
       form: {
-        id:'',
+        id: '',
         raffleticket: '',
-        quality:''
+        // quality:''
       },
-      zdydialog:false,
-      loading: true,
       tableData: [{
         id: '0',
-        raffleticket: '10',
-        quality: '绿券',
-      },],
+        raffleticket: '[100,100,100]',
+        // quality: '绿券',
+      }, {
+        id: '1',
+        raffleticket: '[10,20,30]',
+      }],
+      zdydialog: false,
+      loading: true,
+      submitType: "",
     }
   },
 
   methods: {
-    add(){
-      this.form ={
-        id:'',
-        raffleticket:'',
-        quality:''
+    add() {
+      this.form = {
+        id: '',
+        raffleticket: '',
+        quality: ''
       }
       this.zdydialog = false
       this.submitType = "add"
     },
     submit() {
-      if(this.submitType == "add"){
+      if (this.submitType == "add") {
         this.tableData.push(this.form)//向tableData中添加数据
         this.zdydialog = false
-      }else{
+      } else {
 
       }
     },
     cancel() {
       this.zdydialog = false
     },
-
+    handleEdit(index, row) {
+      this.form = row     //将该行对象数据直接赋给form
+      this.zdydialog = false //自定义对话框展示
+      this.submitType = "update";
+    },
+    handleDelete(index, row) {
+      this.$confirm('此操作将删除改操作, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.tableData.splice(index, 1)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
   },
   //监听两个输入框的值
   watch: {
-    form:{
-      handler(newVal,oldVal){
-        this.form.quality = this.form.raffleticket;
+    form: {
+      handler(newVal, oldVal) {
+        // this.form.quality = this.form.raffleticket;
 
       },
-      deep:true
+      deep: true
     }
   }
 
 }
 </script>
+<style>
+.tableAll {
+  /* margin-left: 100px; */
+  width: 100%;
+  height: 100vh;
+  /* border: red 1px solid; */
+}
+
+.addArea {
+  height: 40px;
+  width: 920px;
+  /* border: red 1px solid; */
+}
+
+.add_btn {
+  float: left;
+}
+
+.dialog_input {
+  width: 220px;
+}
+</style>
